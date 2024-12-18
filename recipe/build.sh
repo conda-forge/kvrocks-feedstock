1c84d8ba5e049a3cc6dcdb1e5073f3c94f74e50c
Twice <twice@apache.org>
2024-12-18 16:03:56 +0800

    Make the build portable and add license files

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-./x.py build -DENABLE_LUAJIT=OFF -DENABLE_OPENSSL=ON 
+./x.py build -DENABLE_LUAJIT=OFF -DENABLE_OPENSSL=ON -DPORTABLE=1
 
 mkdir -p "${PREFIX}/etc"
 
@@ -6,3 +6,8 @@ cp build/kvrocks "${PREFIX}/bin/kvrocks"
 cp build/kvrocks2redis "${PREFIX}/bin/kvrocks2redis"
 cp kvrocks.conf "${PREFIX}/etc/kvrocks.conf"
 
+mkdir -p "${PREFIX}/share/kvrocks"
+
+cp LICENSE "${PREFIX}/share/kvrocks/"
+cp NOTICE "${PREFIX}/share/kvrocks/"
+cp -r licenses "${PREFIX}/share/kvrocks/"
